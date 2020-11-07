aa18be364224cb6314d2731f93f5d7802c9f1d43
arpan13 <arpankumarpal728@gmail.com>
2020-11-07 21:02:46 +0530

    Completed display all employee and manager form the database

--- src/com/hotelmanagementsystem/Reception.java
+++ src/com/hotelmanagementsystem/Reception.java
@@ -2,8 +2,10 @@ package com.hotelmanagementsystem;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Reception extends JFrame {
+public class Reception extends JFrame implements ActionListener {
 
     JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12;
 
@@ -15,7 +17,7 @@ public class Reception extends JFrame {
         b1.setBackground(Color.BLACK);
         b1.setForeground(Color.white);
         b1.setBounds(10,30,200,30);
-        //b1.addActionListener(this);
+        b1.addActionListener(this);
         add(b1);
 
 
@@ -23,7 +25,7 @@ public class Reception extends JFrame {
         b2.setBackground(Color.BLACK);
         b2.setForeground(Color.white);
         b2.setBounds(10,70,200,30);
-        //b1.addActionListener(this);
+        b2.addActionListener(this);
         add(b2);
 
 
@@ -31,7 +33,7 @@ public class Reception extends JFrame {
         b3.setBackground(Color.BLACK);
         b3.setForeground(Color.white);
         b3.setBounds(10,110,200,30);
-        //b1.addActionListener(this);
+        b3.addActionListener(this);
         add(b3);
 
 
@@ -39,7 +41,7 @@ public class Reception extends JFrame {
         b4.setBackground(Color.BLACK);
         b4.setForeground(Color.white);
         b4.setBounds(10,150,200,30);
-        //b1.addActionListener(this);
+        b4.addActionListener(this);
         add(b4);
 
 
@@ -47,7 +49,7 @@ public class Reception extends JFrame {
         b5.setBackground(Color.BLACK);
         b5.setForeground(Color.white);
         b5.setBounds(10,190,200,30);
-        //b1.addActionListener(this);
+        b5.addActionListener(this);
         add(b5);
 
 
@@ -55,7 +57,7 @@ public class Reception extends JFrame {
         b6.setBackground(Color.BLACK);
         b6.setForeground(Color.white);
         b6.setBounds(10,230,200,30);
-        //b1.addActionListener(this);
+        b6.addActionListener(this);
         add(b6);
 
 
@@ -63,7 +65,7 @@ public class Reception extends JFrame {
         b7.setBackground(Color.BLACK);
         b7.setForeground(Color.white);
         b7.setBounds(10,270,200,30);
-        //b1.addActionListener(this);
+        b7.addActionListener(this);
         add(b7);
 
 
@@ -71,7 +73,7 @@ public class Reception extends JFrame {
         b8.setBackground(Color.BLACK);
         b8.setForeground(Color.white);
         b8.setBounds(10,310,200,30);
-        //b1.addActionListener(this);
+        b8.addActionListener(this);
         add(b8);
 
 
@@ -79,7 +81,7 @@ public class Reception extends JFrame {
         b9.setBackground(Color.BLACK);
         b9.setForeground(Color.white);
         b9.setBounds(10,350,200,30);
-        //b1.addActionListener(this);
+        b9.addActionListener(this);
         add(b9);
 
 
@@ -87,7 +89,7 @@ public class Reception extends JFrame {
         b10.setBackground(Color.BLACK);
         b10.setForeground(Color.white);
         b10.setBounds(10,390,200,30);
-        //b1.addActionListener(this);
+        b10.addActionListener(this);
         add(b10);
 
 
@@ -95,15 +97,15 @@ public class Reception extends JFrame {
         b11.setBackground(Color.BLACK);
         b11.setForeground(Color.white);
         b11.setBounds(10,430,200,30);
-        //b1.addActionListener(this);
+        b11.addActionListener(this);
         add(b11);
 
 
         b12=new JButton("Logout");
         b12.setBackground(Color.BLACK);
         b12.setForeground(Color.white);
-        b12.setBounds(10,570,200,30);
-        //b1.addActionListener(this);
+        b12.setBounds(10,470,200,30);
+        b12.addActionListener(this);
         add(b12);
 
 
@@ -123,9 +125,39 @@ public class Reception extends JFrame {
 
         getContentPane().setBackground(Color.white);
         setLayout(null);
-        setBounds(350,200,800,570);
+        setBounds(350,200,800,600);
         setVisible(true);
 
+    }
+    public void actionPerformed(ActionEvent ae){
+
+        if(ae.getSource()==b1){
+
+        }else if(ae.getSource()==b2){
+
+        }else if(ae.getSource()==b3){
+
+        }else if(ae.getSource()==b4){
+            new EmployeeInfo().setVisible(true);
+            this.setVisible(false);
+        }else if(ae.getSource()==b5){
+
+        }else if(ae.getSource()==b6){
+            new ManagerInfo().setVisible(true);
+            this.setVisible(false);
+        }else if(ae.getSource()==b7){
+
+        }else if(ae.getSource()==b8){
+
+        }else if(ae.getSource()==b9){
+
+        }else if(ae.getSource()==b10){
+
+        }else if(ae.getSource()==b11){
+
+        }else if(ae.getSource()==b12){
+            setVisible(false);
+        }
     }
 
 
